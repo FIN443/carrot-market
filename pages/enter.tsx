7182ab95a00e45f450e0fc906dbd7b08f5018bab
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-02-18 16:02:38 +0900

    Layout

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,26 +1,24 @@
+import type { NextPage } from "next";
 import { useState } from "react";
+import { cls } from "../libs/utils";
 
-function cls(...classnames: string[]) {
-  return classnames.join(" ");
-}
-
-export default function Enter() {
+const Enter: NextPage = () => {
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => setMethod("email");
   const onPhoneClick = () => setMethod("phone");
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
-      <div className="mt-8">
+      <div className="mt-12">
         <div className="flex flex-col items-center">
           <h5 className="text-sm text-gray-500 font-medium">Enter using:</h5>
-          <div className="grid border-b w-full mt-8 grid-cols-2 gap-16">
+          <div className="grid  border-b  w-full mt-8 grid-cols-2 ">
             <button
               className={cls(
-                "pb-4 font-medium border-b-2",
+                "pb-4 font-medium text-sm border-b-2",
                 method === "email"
-                  ? "border-orange-500 text-orange-400"
-                  : "border-transparent text-gray-500"
+                  ? " border-orange-500 text-orange-400"
+                  : "border-transparent hover:text-gray-400 text-gray-500"
               )}
               onClick={onEmailClick}
             >
@@ -28,10 +26,10 @@ export default function Enter() {
             </button>
             <button
               className={cls(
-                "pb-4 font-medium border-b-2",
+                "pb-4 font-medium text-sm border-b-2",
                 method === "phone"
-                  ? "border-orange-500 text-orange-400"
-                  : "border-transparent text-gray-500"
+                  ? " border-orange-500 text-orange-400"
+                  : "border-transparent hover:text-gray-400 text-gray-500"
               )}
               onClick={onPhoneClick}
             >
@@ -67,7 +65,7 @@ export default function Enter() {
               </div>
             ) : null}
           </div>
-          <button className="mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
+          <button className="mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
             {method === "email" ? "Get login link" : null}
             {method === "phone" ? "Get one-time password" : null}
           </button>
@@ -75,7 +73,7 @@ export default function Enter() {
         <div className="mt-8">
           <div className="relative">
             <div className="absolute w-full border-t border-gray-300" />
-            <div className="relative -top-3 text-center">
+            <div className="relative -top-3 text-center ">
               <span className="bg-white px-2 text-sm text-gray-500">
                 Or enter with
               </span>
@@ -111,4 +109,5 @@ export default function Enter() {
       </div>
     </div>
   );
-}
+};
+export default Enter;
