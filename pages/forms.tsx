554cf35fc04e9544a2582982d7c3d4fab031997c
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-02-21 14:24:15 +0900

    Practice ReactHookForm

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,20 +1,78 @@
 import React, { useState } from "react";
+import { FieldErrors, useForm } from "react-hook-form";
 
 // Why use React Hook Form
-// Less code
-// Better validation
+// Less code (v)
+// Better validation (v)
 // Better Errors (set, clear, display)
 // Have control over inputs
-// Dont deal with events
-// Easier Inputs
+// Dont deal with events (v)
+// Easier Inputs (v)
+
+interface ILoginForm {
+  username: string;
+  email: string;
+  password: string;
+  errors?: string;
+}
 
 export default function Forms() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    setError,
+    setValue,
+    reset,
+    resetField,
+  } = useForm<ILoginForm>({
+    mode: "onBlur",
+  });
+  const onValid = (data: ILoginForm) => {
+    console.log("im valid");
+    setError("errors", { message: "Backend is offline sorry" });
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
   return (
-    <form>
-      <input type="text" placeholder="Username" required />
-      <input type="email" placeholder="Email" required />
-      <input type="password" placeholder="Password" required />
-      <input type="submit" value="Create Account" />
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })}
+        type="email"
+        placeholder="Email"
+        className={`${Boolean(errors.email) ? "border-red-500" : ""}`}
+      />
+      {errors.email?.message}
+      <input
+        {...register("password", { required: "Password is required" })}
+        type="password"
+        placeholder="Password"
+      />
+      <input
+        className="border border-gray-500 p-2 "
+        type="submit"
+        value="Create Account"
+      />
+      {errors.errors?.message}
     </form>
   );
 }
