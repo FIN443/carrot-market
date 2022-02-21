d5aab230dcda245429d3b2f0b3bbcbfd56cbc887
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-02-21 22:20:22 +0900

    Accounts Token

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,61 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const token = await client.token.create({
+    data: {
+      payload: "1234",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
+    },
+  });
+  console.log(token);
+  /* if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (user) console.log("founded");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email: email,
+        },
+      });
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log("founded");
+    if (!user) {
+      console.log("Did not find. Will create.");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      });
+    }
+    console.log(user);
+  } */
   return res.status(200).end();
 }
 
