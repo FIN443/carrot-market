eaa1292ed58730124509a6338036f7260cc4242d
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-02-25 14:52:33 +0900

    useUser Refactor to SWR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="w-full max-w-xl mx-auto">
-      <Component {...pageProps} />
+      {/* Global에 useSWR fetcher... 등 함수 기본 설정 */}
+      <SWRConfig
+        value={{
+          // refreshInterval: 2000,
+          fetcher: (url: string) =>
+            fetch(url).then((response) => response.json()),
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
     </div>
   );
 }
