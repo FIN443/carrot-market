39d76389febee86fd110ffc9ac0b9cd1563208c2
FIN443 <70796031+FIN443@users.noreply.github.com>
2022-03-15 13:46:58 +0900

    Streams Detail Page

---
+++ pages/api/streams/[id].ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+  const stream = await client.stream.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+  });
+  if (!stream) {
+    return res.status(404).json({ ok: false, error: "Stream not found." });
+  }
+  res.json({
+    ok: true,
+    stream,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
